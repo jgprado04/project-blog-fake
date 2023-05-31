2e529738d3fe472a89e228000db5c7c942d991a3
JG <jgprado04@gmail.com>
2023-05-31 14:37:14 -0300

    Projet after Insire (Complete)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // https://jsonplaceholder.typicode.com/posts
 
-async function doInsire() {
+async function doRead() {
     let textArea = document.querySelector(".posts");
     textArea.innerHTML = "Carregando..."
 
@@ -17,4 +17,43 @@ async function doInsire() {
 
 }
 
-doInsire()
+document.querySelector("#insertButton").addEventListener('click', () => {
+    let titleField = document.querySelector("#titleField");
+    let bodyField = document.querySelector("#bodyField");
+
+
+    let title = document.querySelector("#titleField").value;
+    let body = document.querySelector("#bodyField").value;
+
+    if (title && body) {
+        doInsire(title, body, titleField, bodyField);
+    } else {
+        alert("Insira um titulo e um texto no area certa!")
+    }
+
+})
+
+async function doInsire(title, body, titleField, bodyField) {
+    await fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title,
+            body,
+            userId: 2
+        })
+    });
+
+    doRead()
+
+    titleField.value = "";
+    bodyField.value = "";
+
+
+}
+
+
+
+doRead()
