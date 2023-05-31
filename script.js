29d85f5c1e574c80f39c8498fbfe56a3e5c9a69d
JG <jgprado04@gmail.com>
2023-05-31 14:05:30 -0300

    Commit after doRead

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 // https://jsonplaceholder.typicode.com/posts
 
+async function doInsire() {
+    let textArea = document.querySelector(".posts");
+    textArea.innerHTML = "Carregando..."
+
+    let response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    let json = await response.json()
+
+    if (json.length > 0) {
+        textArea.innerHTML = "";
+
+        for (let i in json) {
+            textArea.innerHTML += `<div><h1>${json[i].title}</h1>${json[i].body}<hr/></div>`
+        }
+    }
+
+}
+
+doInsire()
